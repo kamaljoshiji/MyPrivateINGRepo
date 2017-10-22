61e705079c443fa618fa2afb395912c32e345810
kamaljoshiji <kamal.joshi@hcl.com>
2017-10-22 16:38:18 +0200

    removed proprietary Aegon comments

--- HasMap.java
+++ HasMap.java
@@ -1,15 +1,3 @@
-/*------------------------------------------------------------------------------
- *     Ident: Java Education Center
- *    Author: wingerma, neerpati
- * Copyright: (c) 1-apr-1014 Sogeti Nederland B.V. All Rights Reserved.
- *------------------------------------------------------------------------------
- * Sogeti Nederland B.V.    |  No part of this file may be reproduced or
- * Divisie Managed Delivery |  transmitted in any form or by any means,
- * Lange Dreef 17           |  electronic or mechanical, for the purpose,
- * 4131 NJ VIANEN           |  without the express written permission of the
- * The Netherlands          |  copyright holder.
- *------------------------------------------------------------------------------
- */
 package nl.aegon.assessment.cases.helpers;
 
 import java.util.HashMap;
